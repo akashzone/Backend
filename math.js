d2c7c084f6157ba3c11bfcb8620b56272a80cdb2
akashzone <akashnadar.dev@gmail.com>
2025-08-07 18:03:06 +0530

    module.exports for files in NodeJS

---
+++ math.js
@@ -0,0 +1,7 @@
+
+
+exports.Pi = 3.14;
+exports.g = 9.8;
+exports.sum = (a,b)=> a+b;
+
+// module.exports = "123";
--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 
+// let args = process.argv;
 
-let args = process.argv;
+// for(let i=1;i<args.length;i++){
+//      console.log("Hello! ",args[i]);
+// }
 
-for(let i=1;i<args.length;i++){
-     console.log("Hello! ",args[i]);
-}
+let math = require("./math.js");
+console.log(math.Pi);
+console.log(math.g);
+console.log(math.sum(4,3));
+
