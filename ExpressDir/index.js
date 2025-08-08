f63924f93fcde3a5ba0f67800ff50755d4c71613
akashzone <akashnadar.dev@gmail.com>
2025-08-08 22:32:08 +0530

    app.use app.get in ExpressJS

--- ExpressDir/index.js
+++ ExpressDir/index.js
@@ -4,6 +4,15 @@ const app = express();
 console.dir(app);
 let port = 8080;
 
-app.listen(port,()=>{
-    console.log("Hii , I'm listening.");
+// app.listen(port,()=>{
+//     app.use((req,res)=>{
+//     res.send("This is a basic response!");
+// });
+// })
+
+app.get('/apple',(req,res)=>{
+    res.send({
+        name:"Apple",
+        color: "red",
+    });
 })
